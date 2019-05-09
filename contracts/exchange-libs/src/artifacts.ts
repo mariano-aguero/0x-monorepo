801c39526cd3de8716f27916c3e5bd6b7bedb043
Lawrence Forman <lawrence@0xproject.com>
2019-05-09 15:46:09 -0400

    `@0x/contracts-exchange-libs`: Add `takerFeeAssetData` and `makerFeeAssetData` to `Order` struct.

--- contracts/exchange-libs/src/artifacts.ts
+++ contracts/exchange-libs/src/artifacts.ts
@@ -6,7 +6,6 @@
 import { ContractArtifact } from 'ethereum-types';
 
 import * as LibAbiEncoder from '../generated-artifacts/LibAbiEncoder.json';
-import * as LibConstants from '../generated-artifacts/LibConstants.json';
 import * as LibEIP712ExchangeDomain from '../generated-artifacts/LibEIP712ExchangeDomain.json';
 import * as LibFillResults from '../generated-artifacts/LibFillResults.json';
 import * as LibMath from '../generated-artifacts/LibMath.json';
@@ -15,11 +14,10 @@ import * as LibZeroExTransaction from '../generated-artifacts/LibZeroExTransacti
 import * as TestLibs from '../generated-artifacts/TestLibs.json';
 export const artifacts = {
     LibAbiEncoder: LibAbiEncoder as ContractArtifact,
-    LibConstants: LibConstants as ContractArtifact,
+    LibEIP712ExchangeDomain: LibEIP712ExchangeDomain as ContractArtifact,
     LibFillResults: LibFillResults as ContractArtifact,
     LibMath: LibMath as ContractArtifact,
     LibOrder: LibOrder as ContractArtifact,
     LibZeroExTransaction: LibZeroExTransaction as ContractArtifact,
-    LibEIP712ExchangeDomain: LibEIP712ExchangeDomain as ContractArtifact,
     TestLibs: TestLibs as ContractArtifact,
 };
