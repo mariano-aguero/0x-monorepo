91a5fae4711782bd50c92935f357003c977e22bb
David Sun <davidsun@Davids-MacBook-Pro-2.local>
2019-05-20 10:18:21 -0700

    changed showcase image

--- packages/website/ts/components/showcase_section.tsx
+++ packages/website/ts/components/showcase_section.tsx
@@ -15,10 +15,10 @@ const ShowcaseImg = styled.img`
     position: absolute;
     top: 0;
     bottom: 0;
-    right: 0;
+    right: -20%;
     margin: auto 0 auto 0;
-    width: 40%;
-    max-width: 600px;
+    width: 60%;
+    max-width: 900px;
     height: 100%;
     @media (max-width: 900px) {
         display: none;
@@ -49,11 +49,14 @@ const Wrap = styled.section<WrapProps>`
 
 const ContentWrap = styled.div`
     width: 60%;
+    @media (max-width: 900px) {
+        width: 100%;
+    }
 `;
 
 export const ShowcaseSection = (props: Props) => (
     <Wrap paddingMobile={props.paddingMobile} sectionPadding={props.sectionPadding}>
-        <MobileImg src={`${props.showcaseImgSrc.slice(0, -4)}_mobile.png`} />
+        <MobileImg src={props.showcaseImgSrc} />
         <Section maxWidth={props.maxWidth} padding={'80px 0'}>
             <ContentWrap>{props.children}</ContentWrap>
         </Section>
